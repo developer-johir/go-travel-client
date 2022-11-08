8c45ab4c48bed143e2be1acfb172cc9860e933ed
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-11-08 23:06:16 +0600

    firebase deploy complete

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,27 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import ServicesCard from './ServicesCard';
+import React, { useEffect, useState } from "react";
+import ServicesCard from "./ServicesCard";
 
 const Services = () => {
-    const [services, setServices] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:5000/services')
-        .then(res => res.json())
-        .then(data => setServices(data))
-    }, [])
-    return (
-        <div className='py-10 text-center '>
-            <h2>This is Services: {services.length}</h2>
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("https://go-travel-server.vercel.app/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div className="py-10 text-center ">
+      <h2>This is Services: {services.length}</h2>
 
-            <div>
-                {
-                    services.map(service => <ServicesCard
-                        key={service._id}
-                        service={service}
-                    ></ServicesCard>)
-                }
-            </div>
-        </div>
-    );
+      <div>
+        {services.map((service) => (
+          <ServicesCard key={service._id} service={service}></ServicesCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
