b5b849bfa52627db075ecaf340fa644b218c73be
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-11-09 22:00:27 +0600

    Services page complete

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ThereService from '../HomeService/ThereService';
 import Slider from '../Slider/Slider';
+import WhyTravel from '../WhyTravel/WhyTravel';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
             <ThereService></ThereService>
+            <WhyTravel></WhyTravel>
         </div>
     );
 };
---
+++ src/Pages/Home/WhyTravel/WhyTravel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WhyTravel = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default WhyTravel;
